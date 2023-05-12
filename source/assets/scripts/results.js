bb840b367868e61abbc2a4eb0c766c1e01740895
Henry Liu <11.henrybliu@gmail.com>
2023-05-12 15:51:49 -0700

    blueprint for results.js

---
+++ source/assets/scripts/results.js
@@ -0,0 +1,19 @@
+// Pull from an array containing all the cards that were chosen
+const chosenCards = [];
+
+// Get the container elements for each card
+const cardContainers = document.getElementsByClassName('card');
+
+// Loop through the chosenCards array to update each card
+for (let i = 0; i < chosenCards.length; i++) {
+  const card = chosenCards[i];
+  const cardContainer = cardContainers[i];
+  const cardImg = cardContainer.querySelector('img');
+  const cardName = cardContainer.querySelector('h1');
+  const cardDesc = cardContainer.querySelector('p');
+
+  // Display card, image, name, and description
+  cardImg.src = card.image;
+  cardName.textContent = card.name;
+  cardDesc.textContent = card.description;
+}
