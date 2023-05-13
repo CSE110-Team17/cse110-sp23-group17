14a94d9aa2919c115655bcff34d4c362f3d8dfdf
Jenny Quach <65835856+hoatuyet423@users.noreply.github.com>
2023-05-12 17:31:14 -0700

    add eventListener for card

--- source/assets/scripts/game.js
+++ source/assets/scripts/game.js
@@ -1,7 +1,18 @@
 import tarotConfig from '/source/assets/tarot.json' assert { type: 'json' };
 
+ console.log("hello");
+
 const tarotCardNames = [];
 tarotConfig['tarot'].forEach(element => {
     tarotCardNames.push(element['name']);
 });
 console.log(tarotCardNames);
+
+var card = document.getElementsByClassName("card");
+
+function play(event){
+    console.log(event.target.value);
+}
+
+card.eventListener('click', play);
+
