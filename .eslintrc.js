1594247fcbbedbd444d9cf3ed81a5b0b83857b64
Kevin Kang <kevinkang513@gmail.com>
2023-05-30 19:03:00 -0700

    added eslint-action branch to linter action

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
-  "env": {
-    "browser": true,
+    "env": {
+        "browser": true,
         "es2021": true
     },
     "extends": "eslint:recommended",
     "overrides": [
     ],
-    "parser": "babel-eslint",
+    "parser": "babel-eslint"
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
