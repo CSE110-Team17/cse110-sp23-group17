5981f2d557556e5b645769cf7984a9c62f7ad5e7
Steven Hsu <sthsu@ucsd.edu>
2023-06-08 14:20:51 -0700

    test

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -3,12 +3,6 @@ module.exports = {
     headless: process.env.CI === "true",
     ignoreDefaultArgs: ["--disable-extensions"],
     args: ["--no-sandbox"],
-    executablePath: "chrome.exe"
-  },
-  testEnvironment: "jsdom",
-  preset: "ts-jest",
-  transform: {
-    "^.+\\.(ts|tsx)?$": "ts-jest",
-    "^.+\\.(js|jsx)$": "babel-jest",
+    executablePath: "chrome.exe",
   },
 };
