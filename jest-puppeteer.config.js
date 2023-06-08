d480faa71746459ad6a9719b694446bc97edbdfd
Steven Hsu <sthsu@ucsd.edu>
2023-06-08 14:26:11 -0700

    test

--- jest-puppeteer.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  launch: {
-    headless: process.env.CI === "true",
-    ignoreDefaultArgs: ["--disable-extensions"],
-    args: ["--no-sandbox"],
-    executablePath: "chrome.exe",
-  },
-};
