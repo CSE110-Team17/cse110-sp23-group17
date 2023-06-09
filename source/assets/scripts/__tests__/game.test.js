a239caca16ef59fb874bce4191da423cc928aed8
Steven Hsu <sthsu@ucsd.edu>
2023-06-09 10:33:26 -0700

    added more E2E and unit testing

--- source/assets/scripts/__tests__/game.test.js
+++ source/assets/scripts/__tests__/game.test.js
@@ -2,13 +2,13 @@
  * @jest-environment puppeteer
  */
 
+const puppeteer = require("puppeteer");
+
 /**
  * E2E testing of user interactions
  */
-const puppeteer = require("puppeteer");
 describe("E2E button/link testing", () => {
   beforeEach(async () => {
-    // await puppeteer.launch({ headless: false, slowMo: 25,});
     await page.goto(
       "https://cse110-team17.github.io/cse110-sp23-group17/source/game.html#"
     );
@@ -24,21 +24,8 @@ describe("E2E button/link testing", () => {
     expect(url).toEqual(expectedPageLink);
   }, 5000);
 
-  // it("Make sure clicking the X label goes back to game page", async () => {
-  //   //click how to play for popup box
-  //   let popupLink = "#popup-box";
-  //   const tutorialButton = await page.$(`a[href='${popupLink}']`);
-  //   await Promise.all([page.waitForNavigation(), tutorialButton.click()]);
-  //   //click the x to close
-  //   const xButton = await page.$(".box-close");
-  //   await Promise.all([page.waitForNavigation(), xButton.click()]);
-  //   let expectedPageLink =
-  //     "https://cse110-team17.github.io/cse110-sp23-group17/source/game.html#";
-  //   const url = await page.evaluate(() => document.location.href);
-  //   expect(url).toEqual(expectedPageLink);
-  // }, 5000);
-
   it("Check clicking one div btn outputs status", async () => {
+    await page.reload();
     const card = await page.$("#card-1");
     await card.click();
     const status = await page.$eval("#card-1 img", (el) =>
@@ -48,6 +35,7 @@ describe("E2E button/link testing", () => {
   }, 5000);
 
   it("Check that after clicking 4 cards, cannot click another", async () => {
+    await page.reload();
     const card = await page.$$(".card");
     //click 4 cards
     for (let i = 0; i < 4; i++) {
@@ -61,4 +49,49 @@ describe("E2E button/link testing", () => {
     );
     expect(status).toBe("unclicked");
   }, 5000);
+
+  it("After clicking a card, check text content of message displayed to user", async () => {
+    await page.reload();
+    const card = await page.$("#card-1");
+    await card.click();
+    const textContent = await page.$eval(".oracle .message", (el) => {
+      return el.innerText;
+    });
+    expect(textContent).toBeDefined();
+  }, 5000);
+});
+describe("Testing local storage contents is as expected", () => {
+  beforeEach(async () => {
+    await page.goto(
+      "https://cse110-team17.github.io/cse110-sp23-group17/source/game.html#"
+    );
+  });
+  it("Check local storage contents for luck being transfered to results page", async () => {
+    await page.reload();
+    const card = await page.$$(".card");
+    //click 4 cards
+    for (let i = 0; i < 4; i++) {
+      await card[i].click();
+    }
+    await page.waitForNavigation();
+    const localStorage = await page.evaluate(() =>
+      localStorage.getItem("luck")
+    );
+    expect(typeof parseInt(localStorage) === "number").toBe(true);
+  }, 10000);
+
+  it("Check local storage contents of chosen cards being transfered to results page", async () => {
+    await page.reload();
+    const card = await page.$$(".card");
+    //click 4 cards
+    for (let i = 0; i < 4; i++) {
+      await card[i].click();
+    }
+    await page.waitForNavigation();
+    const localStorage = await page.evaluate(() =>
+      localStorage.getItem("chosenCards")
+    );
+    console.log(JSON.parse(localStorage));
+    expect(typeof JSON.parse(localStorage) === "object").toBe(true);
+  }, 10000);
 });
--- source/assets/scripts/__tests__/unitgame.test.js
+++ source/assets/scripts/__tests__/unitgame.test.js
@@ -4,7 +4,7 @@
 const game = require("../game");
 const fs = require("fs");
 window.document.body.innerHTML = fs.readFileSync("source/game.html");
-require("../game");
+// require("../game");
 
 /**
  * Initialize mock tarot object
@@ -23,5 +23,26 @@ describe("test suite for function getTarotCardName", () => {
   });
   test("check content of names is as expected", () => {
     expect(game.getTarotCardName(tarotConfig)).toEqual(["bob"]);
+  });
+});
+
+/**
+ * Test to see if getBarWidth is consistent
+ */
+describe("test suite for function getBarWidth", () => {
+  const oscillatingBarFill = {
+    getBoundingClientRect: jest.fn(() => ({ width: 100 })),
+  };
+
+  const oscillatingBar = {
+    getBoundingClientRect: jest.fn(() => ({ width: 400 })),
+  };
+
+  // Test case
+  it("should calculate the correct bar width", () => {
+    // Call the function
+    const result = game.getBarWidth(oscillatingBarFill, oscillatingBar);
+    //expected calculation ((25*100) / 400) = 6.25 floored to 6
+    expect(result).toBe(6);
   });
 });
